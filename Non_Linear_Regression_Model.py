e027477115d2b5a8481a7b8f6dd0b8d7ccdd690a
Geonu Kim <rjsdnrlarla@gmail.com>
2020-12-06 20:22:47 +0900

    Non-Linear Regression Added

--- Non_Linear_Regression_Model.py
+++ Non_Linear_Regression_Model.py
@@ -13,7 +13,7 @@ x2 = database[1:,3]/900 # Pressure
 x3 = database[1:,4]/120 # SiH4 sccm
 x4 = database[1:,5]/75 # NH3 sccm
 x5 = database[1:,6]/1500 # N2 sccm
-y = database[1:,7]/100  # Tensile Stress
+y = database[1:,7]  # Tensile Stress
 n = len(y)
 
 X1 = tf.compat.v1.placeholder(tf.float32)
@@ -25,17 +25,17 @@ Y = tf.compat.v1.placeholder(tf.float32)
 
 W1 = tf.Variable(np.random.randn(), name = "W1")
 W2 = tf.Variable(np.random.randn(), name = "W2")
-W3 = tf.Variable(np.random.randn(), name = "W3")
+W3_sub = tf.Variable(np.random.randn(), name = "W3_sub")
+W3_sup = tf.Variable(np.random.randn(), name = "W3_sup")
 W4 = tf.Variable(np.random.randn(), name = "W4")
 W5 = tf.Variable(np.random.randn(), name = "W5")
 b = tf.Variable(np.random.randn(), name = "b")
 
-learning_rate = 0.05
-training_epochs = 700
+learning_rate = 0.03
+training_epochs = 2000
 
 # Hypothesis
-### y_pred = tf.add(tf.add(tf.add(tf.add(tf.add(tf.multiply(X1, W1), tf.multiply(X2, W2)), tf.multiply(X3, W3)), tf(multiply(X4,W4))),tf(multiply(X5,W5))),b)
-y_pred = X1*W1 + X2*W2 + X3*W3 + X4*W4 + X5*W5 + b
+y_pred = X1*W1 + X2*W2 + W3_sub*tf.exp(tf.multiply(W3_sup,X3)) + X4*W4 + X5*W5 + b
 
 # Mean Squared Error Cost Function
 cost = tf.reduce_sum(tf.pow(y_pred - Y, 2)) / (2 * n)
@@ -62,16 +62,17 @@ with tf.compat.v1.Session() as sess:
         if (epoch + 1) % 50 == 0:
             # Calculating the cost a every epoch
             c = sess.run(cost, feed_dict={X1:_x1, X2:_x2,X3:_x3,X4:_x4,X5:_x5, Y:_y})
-            print("Epoch", (epoch + 1), ": cost =", c, "W1 =", sess.run(W1), "W2 =", sess.run(W2),"W3 =", sess.run(W3),"W4 =", sess.run(W4),"W5 =", sess.run(W5),"b =", sess.run(b))
+            print("Epoch", (epoch + 1), ": cost =", c, "W1 =", sess.run(W1), "W2 =", sess.run(W2),"W3_sub =", sess.run(W3_sub),"W3_sup =", sess.run(W3_sup),"W4 =", sess.run(W4),"W5 =", sess.run(W5),"b =", sess.run(b))
 
             # Storing necessary values to be used outside the Session
     training_cost = sess.run(cost, feed_dict={X1:_x1, X2:_x2,X3:_x3,X4:_x4,X5:_x5, Y:_y})
     weight1 = sess.run(W1)
     weight2 = sess.run(W2)
-    weight3 = sess.run(W3)
+    weight3_sub = sess.run(W3_sub)
+    weight3_sup = sess.run(W3_sup)
     weight4 = sess.run(W4)
     weight5 = sess.run(W5)
     bias = sess.run(b)
 
 #Normalized Weights
-print(weight1, weight2, weight3, weight4, weight5, bias)
+print(weight1, weight2, weight3_sub, weight3_sup, weight4, weight5, bias)
